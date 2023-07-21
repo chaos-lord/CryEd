pret/pokecrystal

Require rgbds 0.6.0

includes.asm
@@ -1,6 +1,26 @@
 INCLUDE "charmap.asm"
 
-INCLUDE "macros.asm"
+INCLUDE "macros/asserts.asm"
+INCLUDE "macros/const.asm"
+INCLUDE "macros/predef.asm"
+INCLUDE "macros/farcall.asm"
+INCLUDE "macros/data.asm"
+INCLUDE "macros/code.asm"
+INCLUDE "macros/gfx.asm"
+INCLUDE "macros/coords.asm"
+INCLUDE "macros/vc.asm"
+
+INCLUDE "macros/scripts/audio.asm"
+INCLUDE "macros/scripts/maps.asm"
+INCLUDE "macros/scripts/events.asm"
+INCLUDE "macros/scripts/text.asm"
+INCLUDE "macros/scripts/movement.asm"
+INCLUDE "macros/scripts/battle_commands.asm"
+INCLUDE "macros/scripts/battle_anims.asm"
+INCLUDE "macros/scripts/oam_anims.asm"
+INCLUDE "macros/scripts/pic_anims.asm"
+
+INCLUDE "macros/legacy.asm"
 
 INCLUDE "constants/hardware_constants.asm"
 INCLUDE "constants/deco_constants.asm"

chaos-lord/CryEd

includes.asm
@@ -1,0 +1,22 @@
+INCLUDE "macros/const.asm"
+INCLUDE "macros/data.asm"
+INCLUDE "macros/wram.asm"
+INCLUDE "macros/code.asm"
+INCLUDE "macros/legacy.asm"
+INCLUDE "macros/audio.asm"
+INCLUDE "macros/asserts.asm"
+INCLUDE "macros/copy.asm"
+INCLUDE "macros/gfx.asm"
+
+INCLUDE	"constants/misc_constants.asm"
+INCLUDE	"constants/sfx_constants.asm"
+INCLUDE	"constants/gfx_constants.asm"
+INCLUDE	"constants/text_constants.asm"
+INCLUDE	"constants/battle_constants.asm"
+INCLUDE	"constants/audio_constants.asm"
+INCLUDE	"constants/cry_constants.asm"
+INCLUDE	"constants/music_constants.asm"
+INCLUDE	"constants/hardware_constants.asm"
+INCLUDE	"constants/wram_constants.asm"
+INCLUDE	"constants/input_constants.asm"
+INCLUDE	"constants/pokemon_data_constants.asm"